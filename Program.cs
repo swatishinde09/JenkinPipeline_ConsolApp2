5f69f40b147f1807f05689cd8946e3f09cac707b
swatishinde09 <148969831+swatishinde09@users.noreply.github.com>
2023-11-29 14:47:58 +0530

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace JenkinPipeline_ConsolApp2
+{
+    internal class Program
+    {
+        void show()
+        {
+            int a = 5, b;
+            b = a * a;
+            Console.WriteLine("Square of a="+b);
+
+        }
+        static void Main(string[] args)
+        {
+            Program p= new Program();
+            p.show();
+            Console.ReadLine();
+        }
+    }
+}
